b191d5007b4bf8ef4b9ce49922415f478179c352
taeaeaexin <re_fuse@naver.com>
2025-07-21 10:44:56 +0900

    refactor : KakaoUserInfoDto 분리 #32

--- src/main/java/com/ureca/ocean/jjh/oauth/service/impl/KakaoAuthServiceImpl.java
+++ src/main/java/com/ureca/ocean/jjh/oauth/service/impl/KakaoAuthServiceImpl.java
@@ -6,6 +6,7 @@ import com.ureca.ocean.jjh.common.exception.ErrorCode;
 import com.ureca.ocean.jjh.exception.AuthException;
 import com.ureca.ocean.jjh.oauth.dto.KakaoLoginResultDto;
 import com.ureca.ocean.jjh.oauth.dto.KakaoTokenResponseDto;
+import com.ureca.ocean.jjh.oauth.dto.KakaoUserInfoDto;
 import com.ureca.ocean.jjh.oauth.service.KakaoAuthService;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.*;
@@ -40,7 +41,7 @@ public class KakaoAuthServiceImpl implements KakaoAuthService {
     @Override
     public KakaoLoginResultDto getKakaoLogin(String code) {
         String accessToken = getAccessToken(code);
-        KakaoUserInfo userInfo = getUserInfo(accessToken);
+        KakaoUserInfoDto userInfo = getUserInfo(accessToken);
         String jwt = createJwtToken(userInfo.getEmail());
 
         KakaoLoginResultDto kakaoLoginResultDto = new KakaoLoginResultDto(userInfo.getEmail(), userInfo.getNickname(), jwt);
@@ -87,41 +88,31 @@ public class KakaoAuthServiceImpl implements KakaoAuthService {
         }
     }
 
-    private KakaoUserInfo getUserInfo(String accessToken) {
+    private KakaoUserInfoDto getUserInfo(String accessToken) {
+        String url = "https://kapi.kakao.com/v2/user/me";
+
         HttpHeaders headers = new HttpHeaders();
         headers.setBearerAuth(accessToken);
         headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
 
-        HttpEntity<?> request = new HttpEntity<>(headers);
+        HttpEntity<Void> request = new HttpEntity<>(headers);
 
-        ResponseEntity<String> response = new RestTemplate()
-                .exchange("https://kapi.kakao.com/v2/user/me", HttpMethod.POST, request, String.class);
-
+        // exception 처리
         try {
-            JsonNode jsonNode = objectMapper.readTree(response.getBody());
-            String email = jsonNode.path("kakao_account").path("email").asText();
-            String nickname = jsonNode.path("properties").path("nickname").asText();
-            return new KakaoUserInfo(email, nickname);
+            ResponseEntity<KakaoUserInfoDto> response = new RestTemplate().exchange(
+                    url,
+                    HttpMethod.GET,
+                    request,
+                    KakaoUserInfoDto.class
+            );
+
+            KakaoUserInfoDto userInfo = response.getBody();
+            if (userInfo == null || userInfo.getEmail() == null || userInfo.getNickname() == null) {
+                throw new AuthException(ErrorCode.KAKAO_RESPONSE_FAIL);
+            }
+            return userInfo;
         } catch (Exception e) {
-            throw new RuntimeException("Failed to get user info from Kakao", e);
-        }
-    }
-
-    private static class KakaoUserInfo {
-        private final String email;
-        private final String nickname;
-
-        public KakaoUserInfo(String email, String nickname) {
-            this.email = email;
-            this.nickname = nickname;
-        }
-
-        public String getEmail() {
-            return email;
-        }
-
-        public String getNickname() {
-            return nickname;
+            throw new AuthException(ErrorCode.KAKAO_RESPONSE_FAIL);
         }
     }
 
